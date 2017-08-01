d2cc06c2e56bce2c15c13072366070221646f864
funkybo <esher5580@gmail.com>
2017-08-01 15:50:06 +0300

    gulpfile pug view engine

--- gulpfile.js
+++ gulpfile.js
@@ -15,34 +15,33 @@ let gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     cleanCSS = require('gulp-clean-css'),
     //Other
+    pug = require('gulp-pug'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
     rimraf = require('rimraf'),
     watch = require('gulp-watch');
-
-
 //=================================================================
-
 
 let path = {
     build: {
-        html: 'public/',
+        pug: 'public/',
         js: 'public/js/',
         css: 'public/css/',
         img: 'public/img/',
         fonts: 'public/fonts/'
     },
     src: {
-        html: 'src/**/*.html',
-        jsMain: ['src/js/**/*.js'],
+        pug: 'src/templates/*.pug',
+        js: ['src/js/**/*.js', '!src/js/modernizr.custom.js'],
+        jsModernizr: 'src/js/modernizr.custom.js',
         style: 'src/css/*.scss',
         img: 'src/img/**/*.*',
         spritesStyle: 'src/css/',
         sprites: 'src/img/sprites/*.*',
-        fonts: 'src/fonts/**/*.*'
+        fonts: ['src/fonts/**/*.*', './bower_components/font-awesome/fonts/**/*.{ttf,woff,woff2,eot,otf,svg}']
     },
     watch: {
-        html: 'src/**/*.html',
+        pug: 'src/**/*.pug',
         js: 'src/js/**/*.js',
         style: 'src/css/**/*.scss',
         img: 'src/img/**/*.*',
@@ -52,27 +51,26 @@ let path = {
     clean: './public'
 };
 
-// gulp.task('webserver', function () {
-//     browserSync({
-//         server: "./public"
-//     }, function (err, bs) {
-//         ngrok.connect(bs.options.get('port'), function (err, url) {
-//         });
-//     });
-// });
+gulp.task('webserver', () => {
+    browserSync({
+        server: "./public"
+    }, (err, bs) => {
+        ngrok.connect(bs.options.get('port'), (err, url) => {});
+    });
+});
 
-gulp.task('clean', function (cb) {
+gulp.task('clean', (cb) => {
     rimraf(path.clean, cb);
 });
 
-gulp.task('sprites:build', function() {
+gulp.task('sprites:build', () => {
     let spritesData = gulp.src(path.src.sprites)
         .pipe(spritesmith({
             imgName: '../img/sprites.png',
             cssName: '_sprites.scss',
             cssFormat: 'scss',
             algorithm: 'binary-tree',
-            /*cssVarMap: function(sprites) {
+            /*cssVarMap: (sprites) => {
              sprites.name = 's-' + sprites.name
              }*/
         }));
@@ -81,7 +79,7 @@ gulp.task('sprites:build', function() {
     spritesData.css.pipe(gulp.dest(path.src.spritesStyle));
 });
 
-gulp.task('image:build', function () {
+gulp.task('image:build', () => {
     return gulp.src(path.src.img)
         .pipe(imagemin({
             progressive: true,
@@ -93,19 +91,20 @@ gulp.task('image:build', function () {
         .pipe(reload({stream: true}));
 });
 
-gulp.task('fonts:build', function () {
+gulp.task('fonts:build', () => {
     return gulp.src(path.src.fonts)
         .pipe(gulp.dest(path.build.fonts))
 });
 
-gulp.task('html:build', function () {
-    return gulp.src(path.src.html)
-        .pipe(gulp.dest(path.build.html))
+gulp.task('pug:build', () => {
+    return gulp.src(path.src.pug)
+        .pipe( pug( {pretty: true}))
+        .pipe(gulp.dest(path.build.pug))
         .pipe(reload({stream: true}));
 });
 
-gulp.task('js:build', function () {
-    return gulp.src(path.src.jsMain)
+gulp.task('js:build', () => {
+    return gulp.src(path.src.js)
         .pipe(sourcemaps.init())
         .pipe(concat('script.min.js'))
         .pipe(uglify())
@@ -114,7 +113,13 @@ gulp.task('js:build', function () {
         .pipe(reload({stream: true}));
 });
 
-gulp.task('style:build', function () {
+gulp.task('js:build:modernizr', () => {
+    return gulp.src(path.src.jsModernizr)
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', () => {
     return gulp.src(path.src.style)
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -129,43 +134,48 @@ gulp.task('style:build', function () {
         .pipe(reload({stream: true}));
 });
 
-gulp.task('concatPlug:css', function () {
+let bower = (path) => {
+    return 'bower_components/' + path;
+};
+
+gulp.task('concatPlug:css', () => {
     return gulp.src([
-        'bower_components/normalize-css/normalize.css',
-        'bower_components/font-awesome/css/font-awesome.min.css',
-        'bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css',
-        'bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
-        'bower_components/magnific-popup/dist/magnific-popup.css',
-        'bower_components/DirectionAwareHoverEffect/css/style.css'
+        bower('normalize-css/normalize.css'),
+        bower('font-awesome/css/font-awesome.min.css'),
+        bower('material-design-iconic-font/dist/css/material-design-iconic-font.min.css'),
+        bower('owl.carousel/dist/assets/owl.carousel.min.css'),
+        bower('magnific-popup/dist/magnific-popup.css'),
+        bower('DirectionAwareHoverEffect/css/style.css')
     ])
         .pipe(concat('components.min.css'))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest(path.build.css));
 });
 
-gulp.task('concatPlug:js', function () {
+gulp.task('concatPlug:js', () => {
     return gulp.src([
         // 'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        'bower_components/owl.carousel/dist/owl.carousel.min.js',
-        'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
-        'bower_components/bootstrap-validator/dist/validator.min.js',
-        'bower_components/masonry/dist/masonry.pkgd.min.js',
-        'bower_components/DirectionAwareHoverEffect/js/jquery.hoverdir.js',
-        'bower_components/shufflejs/dist/jquery.shuffle.min.js',
-        'bower_components/imagesloaded/imagesloaded.pkgd.min.js',
-        'bower_components/masonry/dist/masonry.pkgd.min.js'
+        bower('bootstrap-sass/assets/javascripts/bootstrap.min.js'),
+        bower('owl.carousel/dist/owl.carousel.min.js'),
+        bower('magnific-popup/dist/jquery.magnific-popup.min.js'),
+        bower('bootstrap-validator/dist/validator.min.js'),
+        bower('masonry/dist/masonry.pkgd.min.js'),
+        bower('DirectionAwareHoverEffect/js/jquery.hoverdir.js'),
+        bower('shufflejs/dist/jquery.shuffle.min.js'),
+        bower('imagesloaded/imagesloaded.pkgd.min.js'),
+        bower('masonry/dist/masonry.pkgd.min.js')
     ])
         .pipe(concat('components.min.js'))
-        .pipe(uglify().on('error', function(e){
+        .pipe(uglify().on('error', (e) => {
             console.log(e);
         }))
         .pipe(gulp.dest(path.build.js));
 });
 
 gulp.task('build', [
-    'html:build',
+    'pug:build',
     'js:build',
+    'js:build:modernizr',
     'style:build',
     'fonts:build',
     'sprites:build',
@@ -174,25 +184,25 @@ gulp.task('build', [
     'concatPlug:js'
 ]);
 
-gulp.task('watch', function () {
-    watch([path.watch.html], function (event, cb) {
-        gulp.start('html:build');
+gulp.task('watch', () => {
+    watch([path.watch.pug], (event, cb) => {
+        gulp.start('pug:build');
     });
-    watch([path.watch.style], function (event, cb) {
+    watch([path.watch.style], (event, cb) => {
         gulp.start('style:build');
     });
-    watch([path.watch.js], function (event, cb) {
+    watch([path.watch.js], (event, cb) => {
         gulp.start('js:build');
     });
-    watch([path.watch.sprites], function (event, cb) {
+    watch([path.watch.sprites], (event, cb) => {
         gulp.start('sprites:build');
     });
-    watch([path.watch.img], function (event, cb) {
+    watch([path.watch.img], (event, cb) => {
         gulp.start('image:build');
     });
-    watch([path.watch.fonts], function (event, cb) {
+    watch([path.watch.fonts], (event, cb) => {
         gulp.start('fonts:build');
     });
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'webserver', 'watch']);
