fe32abde2fe59ab5fb8740b28a5f8724460200c4
funkybo <esher5580@gmail.com>
2017-08-07 17:38:40 +0300

    google map

---
+++ src/js/partials/google-map.js
@@ -0,0 +1,17 @@
+function myMap() {
+    var lat = 46.4461538,
+        lng = 30.7488844;
+    var myCenter = new google.maps.LatLng(lat, lng);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: myCenter,
+        zoom: 15,
+        styles: [{"featureType": "landscape", "stylers": [{"hue": "#FFBB00"}, {"saturation": 43.400000000000006}, {"lightness": 37.599999999999994}, {"gamma": 1}]}, {"featureType": "road.highway", "stylers": [{"hue": "#FFC200"}, {"saturation": -61.8}, {"lightness": 45.599999999999994}, {"gamma": 1}]}, {"featureType": "road.arterial", "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 51.19999999999999}, {"gamma": 1}]}, {"featureType": "road.local", "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 52}, {"gamma": 1}]}, {"featureType": "water", "stylers": [{"hue": "#0078FF"}, {"saturation": -13.200000000000003}, {"lightness": 2.4000000000000057}, {"gamma": 1}]}, {"featureType": "poi", "stylers": [{"hue": "#00FF6A"}, {"saturation": -1.0989010989011234}, {"lightness": 11.200000000000017}, {"gamma": 1}]}]
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+        position: myCenter,
+        icon: '../../img/map-marker.png'
+    });
+    marker.setMap(map);
+}
